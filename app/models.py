98031f27b1a198d16817c9284be226e9f41d2b94
sheet7dev <kamilsztabinski@gmail.com>
2024-10-22 19:55:00 +0200

    create user and application models

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Application(db.Model):
+    __tablename__ = 'applications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    company_name = db.Column(db.String(255), nullable=False)
+    job_position = db.Column(db.String(255), nullable=False)
+    submission_date = db.Column(db.Date, nullable=False)
+    job_link = db.Column(db.String(500))
+    status = db.Column(db.String(20), default='pending')
