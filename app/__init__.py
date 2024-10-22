e0337c26f8f91afb4565e1ab13e10f0388e47d84
sheet7dev <kamilsztabinski@gmail.com>
2024-10-22 19:55:00 +0200

    app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,24 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
+from flask_cors import CORS
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    CORS(app)
+
+    return app
