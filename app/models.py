ae2e3961b9b2b1a52b932b27387c4ae99dc1db2b
sheet7dev <kamilsztabinski@gmail.com>
2024-10-22 19:55:01 +0200

    update user model relation

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    applications = db.relationship('Application', backref='user', lazy=True)
 
 
     def set_password(self, password):
